66e1df61efbcdcc22491c1c11f3a62bf2f4ec261
tweirsolutions <tim.e.mccarthy@gmail.com>
2022-02-10 20:08:26 +0000

    Create Basic Template.py

---
+++ Basic Template.py
@@ -0,0 +1,25 @@
+
+import sys
+
+#input and output files from first 2 arguments  
+inputfile = sys.argv[1]
+outputfile = sys.argv[2]
+countsfile = sys.argv[3]
+
+#read input file into array
+iFile = open(inputfile, 'r') 
+inputArray = iFile.read().splitlines() 
+iFile.close()
+
+    
+
+oFile = open(outputfile, 'w') #write to file
+for line in inputArray:
+#
+#process the lines here
+    processed = line #simply copies the source string
+#endif    
+#
+    oFile.write(processed+'\n') 
+oFile.close()
+                        
